2a0c53766e832e16642124bc256fc54d6d78a2ea
David <david.cortes.rivera@gmail.com>
2018-09-25 19:09:10 +0200

    add support for mingw compilation in windows

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 import numpy
-import platform
 
-if platform.system() == "Windows":
-	ext_mod = Extension("ctpfrec.cy",
-                             sources=["ctpfrec/cy.pyx"],
-                             include_dirs=[numpy.get_include()],
-                             extra_compile_args=['/openmp', '/O2'])
-else:
-	ext_mod = Extension("ctpfrec.cy",
-                             sources=["ctpfrec/cy.pyx"],
-                             include_dirs=[numpy.get_include()],
-                             extra_compile_args=['-fopenmp', '-O2'],
-                             extra_link_args=['-fopenmp'])
-### Comment: -Ofast gives worse speed than -O2 or -O3
+## https://stackoverflow.com/questions/724664/python-distutils-how-to-get-a-compiler-that-is-going-to-be-used
+class build_ext_subclass( build_ext ):
+  def build_extensions(self):
+    c = self.compiler.compiler_type
+    # TODO: add entries for intel's ICC
+    if c == 'msvc': # visual studio
+      for e in self.extensions:
+        e.extra_compile_args = ['/openmp', '/O2']
+    else: # gcc and clang
+      for e in self.extensions:
+        e.extra_compile_args = ['-fopenmp', '-O3']
+        e.extra_link_args = ['-fopenmp']
+        ### Comment: -Ofast gives worse speed than -O2 or -O3
+    build_ext.build_extensions(self)
 
 setup(
   name = 'ctpfrec',
@@ -28,17 +29,16 @@ setup(
    'cython',
    'hpfrec'
 ],
-  version = '0.1',
+  version = '0.1.1',
   description = 'Collaborative topic Poisson factorization for recommender systems',
   author = 'David Cortes',
   author_email = 'david.cortes.rivera@gmail.com',
   url = 'https://github.com/david-cortes/ctpfrec',
-  download_url = 'https://github.com/david-cortes/ctpfrec/archive/0.1.tar.gz',
   keywords = ['collaborative', 'topic', 'modeling', 'poisson', 'probabilistic', 'non-negative', 'factorization',
               'variational inference', 'collaborative filtering', 'cold-start'],
   classifiers = [],
 
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = [ext_mod,
+  cmdclass = {'build_ext': build_ext_subclass},
+  ext_modules = [Extension("ctpfrec.cy", sources=["ctpfrec/cy.pyx"], include_dirs=[numpy.get_include()]),
     ]
 )
