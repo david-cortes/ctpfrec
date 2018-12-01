58ae00528231905598b6a71966279d605766135c
David <david.cortes.rivera@gmail.com>
2018-12-01 17:18:05 +0200

    ugly fix for msvc

--- setup.py
+++ setup.py
@@ -4,6 +4,32 @@ from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 import numpy
 
+## Note: As of the end of 2018, MSVC is still stuck with OpenMP 2.0 (released 2002), which does not support
+## parallel for loops with unsigend iterators. If you are using a different compiler, this part can be safely removed
+if platform.system() == "Windows":
+  import re
+  fout = open("temp1.txt", "w")
+  with open("ctpfrec\\cy.pyx", "r") as fin:
+      for line in fin:
+          fout.write(re.sub("size_t([^\w])", "long\\1", line))
+  fout.close()
+  fout = open("temp2.txt", "w")
+  with open("ctpfrec\\__init__.py", "r") as fin:
+      for line in fin:
+          fout.write(re.sub("size_t([^\w])", "long\\1", line))
+  fout.close()
+
+  fout = open("ctpfrec\\cy.pyx", "w")
+  with open("temp1.txt", "r") as fin:
+      for line in fin:
+          fout.write(line)
+  fout.close()
+  fout = open("ctpfrec\\__init__.py", "w")
+  with open("temp2.txt", "r") as fin:
+      for line in fin:
+          fout.write(line)
+  fout.close()
+
 ## https://stackoverflow.com/questions/724664/python-distutils-how-to-get-a-compiler-that-is-going-to-be-used
 class build_ext_subclass( build_ext ):
   def build_extensions(self):
@@ -29,7 +55,7 @@ setup(
    'cython',
    'hpfrec'
 ],
-  version = '0.1.3.2',
+  version = '0.1.3.3',
   description = 'Collaborative topic Poisson factorization for recommender systems',
   author = 'David Cortes',
   author_email = 'david.cortes.rivera@gmail.com',
