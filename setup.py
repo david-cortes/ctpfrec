d408441dd89c7ba9acd81dd60272464f3d03bb1a
David Cortes <david.cortes.rivera@gmail.com>
2021-03-06 18:02:37 -0300

    bump version

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
      'cython',
      'hpfrec>=0.2.3'
 ],
-    version = '0.1.11',
+    version = '0.1.12',
     description = 'Collaborative topic Poisson factorization for recommender systems',
     author = 'David Cortes',
     author_email = 'david.cortes.rivera@gmail.com',
