777e131e321c41704a195aa02806ffe3d1d3bd6d
David Cortes <david.cortes.rivera@gmail.com>
2021-04-15 11:51:27 -0400

    solve potential issues with pep517

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
      'cython',
      'hpfrec>=0.2.3'
 ],
-    version = '0.1.12',
+    version = '0.1.13',
     description = 'Collaborative topic Poisson factorization for recommender systems',
     author = 'David Cortes',
     author_email = 'david.cortes.rivera@gmail.com',
