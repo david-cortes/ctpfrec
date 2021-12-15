973daa964ef85b000783f6e54ef8cb959d426e7c
David Cortes <david.cortes.rivera@gmail.com>
2021-12-14 21:15:16 -0300

    more compiler flags

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ class build_ext_subclass( build_ext ):
             self.add_march_native()
             self.add_openmp_linkage()
             self.add_no_math_errno()
+            self.add_no_trapping_math()
 
             for e in self.extensions:
                 # e.extra_compile_args = ['-fopenmp', '-O2', '-march=native', '-std=c99']
@@ -75,6 +76,13 @@ class build_ext_subclass( build_ext ):
                 e.extra_compile_args.append(arg_fnme)
                 e.extra_link_args.append(arg_fnme)
 
+    def add_no_trapping_math(self):
+        arg_fntm = "-fno-trapping-math"
+        if self.test_supports_compile_arg(arg_fntm):
+            for e in self.extensions:
+                e.extra_compile_args.append(arg_fntm)
+                e.extra_link_args.append(arg_fntm)
+
     def test_supports_compile_arg(self, comm):
         is_supported = False
         try:
@@ -114,7 +122,7 @@ setup(
      'cython',
      'hpfrec>=0.2.5'
 ],
-    version = '0.1.15-2',
+    version = '0.1.15-3',
     description = 'Collaborative topic Poisson factorization for recommender systems',
     author = 'David Cortes',
     author_email = 'david.cortes.rivera@gmail.com',
